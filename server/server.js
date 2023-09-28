876f739acae167b280d84824b9ba57eba1832dcf
chris dziedzic <cdziedzic@gmail.com>
2023-09-28 11:59:08 -0400

    added auth middleware to server.js apollo is now working

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 
 const app = express();
@@ -18,7 +19,9 @@ const startApolloServer = async () => {
   
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server, {
+      context: authMiddleware
+    }));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
